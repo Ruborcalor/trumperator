ea2e3a68ffbe70362fb4c22690175a9e010abdd3
Cole Killian <g.cole.killian@gmail.com>
2020-10-04 10:22:08 -0400

    Updates

---
+++ precache-manifest.fb6acf451e6daea4a9fb42a139730e00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f306d30c0da596613b1352e24cbeec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb060b667fccbc3886c7",
+    "url": "/static/css/2.d9e9e0c2.chunk.css"
+  },
+  {
+    "revision": "91aec4ce96e89ebb70ec",
+    "url": "/static/css/main.fdb061f0.chunk.css"
+  },
+  {
+    "revision": "cb060b667fccbc3886c7",
+    "url": "/static/js/2.f41c016a.chunk.js"
+  },
+  {
+    "revision": "b6cc2d646ba63d8a21351dfd59a96669",
+    "url": "/static/js/2.f41c016a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91aec4ce96e89ebb70ec",
+    "url": "/static/js/main.c4a95ad7.chunk.js"
+  },
+  {
+    "revision": "d4138d9f4df7b177af68",
+    "url": "/static/js/runtime-main.6fe24d09.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
